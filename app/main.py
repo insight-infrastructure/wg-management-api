2c577c91719d0f178cc3308b093c53d092df66ae
Richard Mah <dick.mah@gmail.com>
2020-07-28 14:32:32 -0400

    final fixes for v1

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import pickle
 import ipaddress
 from collections import OrderedDict
 from fastapi_utils.tasks import repeat_every
-from wgnlpy import WireGuard
+from wgnlpy import WireGuard, PublicKey
 
 wg = WireGuard()
 app = FastAPI()
@@ -43,9 +43,12 @@ def initialize_state():
 
     # load/init address state
     try:
+        print("Trying to import address state...")
         state_file = open("address_state.pkl", "rb")
         address_state = pickle.load(state_file)
+        print("Existing address state imported!")
     except FileNotFoundError:
+        print("No existing address state. Initializing new state...")
         address_state = OrderedDict()
 
         for addr in network.network.hosts():
@@ -55,15 +58,19 @@ def initialize_state():
 
     # load/init peer state
     try:
+        print("Trying to import peer state...")
         state_file = open("peer_state.pkl", "rb")
         peer_state = pickle.load(state_file)
+        print("Existing peer state imported!")
     except FileNotFoundError:
+        print("No existing peer state. Initializing new state...")
         peer_state = dict()
 
 
 @app.on_event("shutdown")
 @repeat_every(seconds=60 * 5)
 def write_state():
+    print("Writing state...")
     with open("address_state.pkl", "wb") as state_file:
         pickle.dump(address_state, state_file)
 
@@ -75,13 +82,8 @@ def write_state():
 @app.post("/register")
 def register_peer(peer_pub_key: str):
     # check to see if peer already in state
-    try:
-        if peer_state[peer_pub_key]:
-            raise HTTPException(400, "Peer is already registered.")
-
-    except KeyError:
-        # we want a KeyError
-        pass
+    if peer_pub_key in peer_state.keys():
+        raise HTTPException(400, "Peer is already registered.")
 
     response = call_peer_register(peer_pub_key)
     return response
@@ -94,11 +96,11 @@ def confirm_peer(peer_pub_key: str):
     except KeyError:
         raise HTTPException(404, "Peer public key was not found in state.")
 
-    if peer_state[peer_pub_key] is "active":
+    if peer_state[peer_pub_key] == "active":
         raise HTTPException(400, "Peer already confirmed")
 
     try:
-        assert peer_pub_key in wg.get_interface(interface).peers
+        assert PublicKey(peer_pub_key) in wg.get_interface(interface).peers
     except AssertionError:
         raise HTTPException(404, "Peer public key was not found in configuration.")
 
@@ -115,16 +117,18 @@ def deregister_peer(peer_pub_key: str):
         raise HTTPException(404, "Peer public key was not found in state.")
 
     try:
-        assert peer_pub_key in wg.get_interface(interface).peers
+        assert PublicKey(peer_pub_key) in wg.get_interface(interface).peers
     except AssertionError:
         raise HTTPException(404, "Peer public key was not found in configuration.")
 
     try:
         wg.remove_peers(interface, peer_pub_key)
-        assert peer_pub_key not in wg.get_interface(interface).peers
+        assert PublicKey(peer_pub_key) not in wg.get_interface(interface).peers
     except AssertionError:
         raise HTTPException(500, "Peer removal failed. Try again.")
 
+    peer_add = peer_state[peer_pub_key]
+    address_state[peer_add] = "free"
     del peer_state[peer_pub_key]
 
     return "Peer removed"
@@ -137,20 +141,23 @@ def call_peer_register(peer_pub_key):
 
     # find first free address
     for address, state in address_state.items():
-        if state is "free":
+        if state == "free":
             selected = address
             break
 
     try:
-        wg.set_peer(interface, peer_pub_key,
-                    allowedips=str(selected) + "/32",
-                    )
-        assert peer_pub_key in wg.get_interface(interface).peers
+        wg.set_peer(
+            interface, peer_pub_key, allowedips=[str(selected) + "/32"],
+        )
+        assert PublicKey(peer_pub_key) in wg.get_interface(interface).peers
         address_state[selected] = "assigned"
         peer_state[peer_pub_key] = selected
 
-    except Exception:
-        pass
+    except ValueError as e:
+        raise HTTPException(400, str(e))
+
+    except Exception as e:
+        print(e)
 
     response = PeerResponse(
         peerAddress=str(selected) + "/32",
